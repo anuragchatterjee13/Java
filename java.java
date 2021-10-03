6f7d9d0fa57cf3e8bd5570cd3fb9f38d87cb4f6a
anuragchatterjee13 <gruesomeanurag13@gmail.com>
2021-10-04 02:46:49 +0530

    first commit

---
+++ java.java
@@ -0,0 +1,14 @@
+package Lol;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class java {
+
+	@Test
+	void test() {
+		 System.out.println("Hello Java");
+	}
+
+}
